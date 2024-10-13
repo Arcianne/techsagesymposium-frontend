c008714f1e9dcffe113de484a8e41cf6c7bf823e
Arcianne <140175810+Arcianne@users.noreply.github.com>
2024-10-13 23:09:10 +0800

    create: event view page (*ᴗ͈ˬᴗ͈)ꕤ*.ﾟ

--- js/toggle-theme.js
+++ js/toggle-theme.js
@@ -9,7 +9,13 @@ toggleThemeButton.addEventListener("click", themeToggle)
 function themeToggle() {
     if(theme.href == GENERAL.LIGHT_THEME_LINK){
         theme.href = GENERAL.DARK_THEME_LINK
+        localStorage.setItem('theme', 'dark')
     } else {
         theme.href = GENERAL.LIGHT_THEME_LINK
+        localStorage.setItem('theme', 'light')
     }
+}
+
+if(localStorage.getItem('theme') === 'dark') {
+    theme.href = GENERAL.DARK_THEME_LINK
 }
