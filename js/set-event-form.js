2ffe6a51b922eaff50431ee25f50c33326441a9c
Arcianne <140175810+Arcianne@users.noreply.github.com>
2024-11-24 12:55:17 +0800

    update: set-event add: footer on about

--- js/set-event-form.js
+++ js/set-event-form.js
@@ -1,22 +1,15 @@
 const title = document.getElementById("title");
-const attendees = document.getElementById("title");
-const speaker = document.getElementById("title");
-const location = document.getElementById("title");
-const venue = document.getElementById("title");
-const duration = document.getElementById("title");
-const date = document.getElementById("title");
-const email = document.getElementById("title");
+const attendees = document.getElementById("attendees");
+const speakerName = document.getElementById("speaker-name");
+const location1 = document.getElementById("location");
+const venue = document.getElementById("venue");
+const duration = document.getElementById("duration");
+const date = document.getElementById("date");
+const email = document.getElementById("email");
 
 function setEvent(event) {
     event.preventDefault();
-    console.log(title.value)
-    console.log(attendees.value)
-    console.log(speaker.value)
-    console.log(location.value)
-    console.log(venue.value)
-    console.log(duration.value)
-    console.log(date.value)
-    console.log(email.value)
+    
     try {
         axios({
             method: 'post',
@@ -24,8 +17,8 @@ function setEvent(event) {
             data: {
                 title: title.value,
                 attendees: attendees.value,
-                speaker: speaker.value,
-                location: location.value,
+                speaker: speakerName.value,
+                location: location1.value,
                 venue: venue.value,
                 duration: duration.value,
                 date: date.value,
